904bdeea5fe948fe8d29ddcb5b37cd8aa9db92c5
Marina <115695769+Marinakostiuchenk@users.noreply.github.com>
2022-10-21 23:06:32 +0300

    lab_2

---
+++ lab_2/task1.py
@@ -0,0 +1,8 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+sum_ = sum(set(list_))
+len_ = len(set(list_))
+middle_ = round((sum_ / len_), ndigits = 5)
+print(sum_)
+print(len_)
+print(middle_)
+# TODO найти сумму, количество и среднее арифметическое уникальных чисел
